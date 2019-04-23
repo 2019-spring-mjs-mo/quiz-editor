1d3597941ac9eb7412b01104b8f02e5e4732ba9d
zpcoffey <43156754+zpcoffey@users.noreply.github.com>
2019-04-22 20:01:48 -0500

    got it all working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -93,6 +93,22 @@ export class AppComponent implements OnInit {
     });
   }
 
+  saveBatchEdits() {
+
+    const editedQuizzes= this.getEditedQuizzes().map(x => ({
+      name: x.name
+      , originalName: x.name
+      , questions: x.questions
+    }));
+    
+    const addedQuizzes = [];
+
+    this.qSvc.saveQuizzes(editedQuizzes, addedQuizzes).subscribe(
+      numberOfChangedQuizzesSuccessfullySaved => console.log(numberOfChangedQuizzesSuccessfullySaved)
+      , error => console.log(error)
+    );
+  }
+
   cancelBatchEdits() {
     this.loadAllQuizzes();
     this.selectQuiz(undefined);
@@ -103,13 +119,16 @@ export class AppComponent implements OnInit {
   }
 
   get numberOfEditedQuizzes() {
+    return this.getEditedQuizzes().length;
+  }
+
+  getEditedQuizzes() {
     return this.quizzes
       .filter(x =>
         (!x.markedForDelete && x.originalName != "Untitled Quiz")
         && (x.name !== x.originalName || x.questionsChecksum !== x.questions.map(x => x.name).join('~'))
-      ).length;
+      );
   }
-
 
   get numberOfAddedQuizzes() {
     return this.quizzes.filter(x => !x.markedForDelete && x.originalName === "Untitled Quiz").length;
